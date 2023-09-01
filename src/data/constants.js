8fb65f4866f029d7bdfbbd7b720da5aa8bfce424
unknown <hamzarq999@gmail.com>
2023-09-01 18:24:48 +0500

    Added Bio constant

---
+++ src/data/constants.js
@@ -0,0 +1,12 @@
+export const Bio = {
+    name: "Hamza Raza Qureshi",
+    role: [
+        "Programmer",
+        "Frontend Developer",
+        "Backend Developer"
+    ],
+    description: "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    resume: "https://drive.google.com/file/d/1Ai4RHM4TbCBjJIWTtxNy8dPrLAlI_JRO/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/hamza-raza-qureshi-21109a221/",
+    github: "https://github.com/HamzaRazaQureshi",
+};
